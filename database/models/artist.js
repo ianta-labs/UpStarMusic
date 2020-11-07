804c9244a97a5c0f12df727e96d751c7390cbf1f
stefian <v_ianta@yahoo.com>
2020-11-06 23:35:59 -0400

    + // Todo: Create Artist Model // const mongoose = require('mongoose'); const AlbumSchema = require('./album'); const Schema = mongoose.Schema;
    const ArtistSchema = new Schema({
    
    });

--- database/models/artist.js
+++ database/models/artist.js
@@ -1,1 +1,8 @@
 // Todo: Create Artist Model //
+const mongoose = require('mongoose');
+const AlbumSchema = require('./album');
+const Schema = mongoose.Schema;
+
+const ArtistSchema = new Schema({
+
+});
