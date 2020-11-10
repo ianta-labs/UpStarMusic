8897e9b0609017c7bccebb733df66edb6c8dd314
stefian <v_ianta@yahoo.com>
2020-11-10 08:54:22 -0400

    + const MINIMUM_ARTISTS = 200;  // min nr of Artists in the App // const ARTISTS_TO_ADD = 15000;  // How many Artists to add if nr gets lower than MINIMUM_ARTISTS //

--- src/seeds.js
+++ src/seeds.js
@@ -3,8 +3,8 @@ import faker from 'faker';
 import { Db, Server } from 'mongodb';
 import { GENRES } from './constants';
 
-const MINIMUM_ARTISTS = 2;
-const ARTISTS_TO_ADD = 15;
+const MINIMUM_ARTISTS = 200;  // min nr of Artists in the App //
+const ARTISTS_TO_ADD = 15000;  // How many Artists to add if nr gets lower than MINIMUM_ARTISTS //
 
 let artistsCollection;
 const db = new Db('upstar_music', new Server('localhost', 27017));
